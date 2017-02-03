3e0606e82fc4a4b87c58253a126778f0e6e1d375
Nils Helmersson <Nils@Nils-MacBook-Pro.local>
2017-02-03 18:09:48 +0100

    hover

--- labs/main.js
+++ labs/main.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
     $("#content").load("home.html");
 
-    $("a").click(function() {
+    $("a").hover(function() {
     var page = $(this).attr("href");
     $("#content").load(page);
     return false;
